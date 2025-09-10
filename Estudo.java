22640226f1591f85d4a19673e75b7067e7a4147f
Caio Benites <caiolimaben@gmail.com>
2025-09-10 17:44:52 -0400

    Adicionei novo estudo sobre laços em Java

---
+++ Estudo.java
@@ -0,0 +1,93 @@
+    public class Estudo {
+    public static void main(String[] args) {
+
+       int a = 25;
+       int b = 10;
+
+       /*
+       System.out.println(a + b);
+       System.out.println(a - b);
+       System.out.println(a * b);
+       System.out.println(a / b);
+       */
+
+        /* double celsius = 25;
+        double Fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("A temperatura em Fahrenheit é: " + Fahrenheit);
+        */
+
+        /* double raio = 5;
+        double areaCirculo = Math.PI * raio * raio;
+
+        System.out.println("A área do círculo é: " + areaCirculo);
+        */
+
+        int numero = -1;
+
+
+        if (numero < 0){
+            System.out.println("Número é Negativo");
+
+        } else if (numero > 0){
+            System.out.println("Número é positivo");
+        } else {
+            System.out.println("Número é 0");
+        }
+
+    int numeroX = 13;
+
+        if(numeroX % 2 == 0) {
+        System.out.println("O número é par");
+        } else {
+        System.out.println("O número é impar");
+        }
+
+    int idade = 21;
+        if(idade > 18){
+            System.out.println("Pode votar");
+        }else if (idade < 18){
+            System.out.println("É criança, não pode votar");
+        }
+
+        int dia = 7; // você pode mudar para qualquer número de 1 a 7
+
+        switch(dia)
+
+        {
+            case 1:
+                System.out.println("Segunda");
+                break;
+            case 2:
+                System.out.println("Terça");
+                break;
+            case 3:
+                System.out.println("Quarta");
+                break;
+            case 4:
+                System.out.println("Quinta");
+                break;
+            case 5:
+                System.out.println("Sexta");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Número inválido! Digite de 1 a 7.");
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+    }
